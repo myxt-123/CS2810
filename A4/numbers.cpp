434ed2b52f3766341181dff443639cd5020f601b
Soumith Basina <soumith.b@gmail.com>
2020-02-04 21:10:20 +0530

    modified numbers.cpp

--- A4/numbers.cpp
+++ A4/numbers.cpp
@@ -1,7 +1,38 @@
 #include <iostream>
 #include <string>
+#include <math.h>
+
+#define M 1000000007
 
 using namespace std;
+
+int gcd(int a, int b)
+{
+    if (b == 0)
+        return a;
+
+    return gcd(b, a % b);
+}
+
+int gcdExtended(int a, int b, int *x, int *y)
+{
+    // Base Case
+    if (a == 0)
+    {
+        *x = 0, *y = 1;
+        return b;
+    }
+
+    int x1, y1; // To store results of recursive call
+    int gcd = gcdExtended(b % a, a, &x1, &y1);
+
+    // Update x and y using results of recursive
+    // call
+    *x = y1 - (b / a) * x1;
+    *y = x1;
+
+    return gcd;
+}
 
 class Complex
 {
@@ -15,20 +46,27 @@ public:
     {
         real = real + n.real;
         img = img + n.img;
-        cout << real << " " << img << endl;
     };
     void sub(Complex n)
     {
         real = real - n.real;
         img = img - n.img;
-        cout << real << " " << img << endl;
     };
     void mult(Complex n)
     {
         double n1, n2;
         n1 = real * n.real - img * n.img;
         n2 = real * n.img + img * n.real;
-        cout << n1 << " " << n2 << endl;
+        real = n1;
+        img = n2;
+    };
+    double getReal()
+    {
+        return real;
+    };
+    double getImg()
+    {
+        return img;
     };
 
 private:
@@ -36,17 +74,34 @@ private:
     double img;
 };
 
-class Rational: public Complex
+class Rational : public Complex
 {
 public:
     Rational(int a, int b)
+        : Complex((double)a / b, 0)
     {
         num = a;
         den = b;
     };
+    int getNum()
+    {
+        return num;
+    };
+    int getDen()
+    {
+        return den;
+    };
     void reduce()
     {
-
+        int k = gcd(getNum(), getDen());
+        num = num / k;
+        den = den / k;
+        if (num > 0 && den < 0)
+        {
+            num = -num;
+            den = -den;
+        }
+        cout << num << " " << den << endl;
     };
 
 private:
@@ -54,12 +109,39 @@ private:
     int den;
 };
 
-class Natural: public Rational
+class Natural : public Rational
 {
 public:
-    Natural();
-    void isPrime();
-    void inverse();
+    Natural(int a)
+        : Rational(a, 1)
+    {
+        nat = a;
+    };
+    void isPrime()
+    {
+        for (int i = 2; i <= sqrt(nat); i++)
+        {
+            if (nat % i == 0)
+            {
+                cout << "0" << endl;
+                return;
+            }
+        }
+        cout << "1" << endl;
+    };
+    void inverse()
+    {
+        int x, y;
+        int g = gcdExtended(nat, M, &x, &y);
+        if (g != 1)
+            return;
+        else
+        {
+            // m is added to handle negative x
+            int res = (x % M + M) % M;
+            cout << res << endl;
+        }
+    }
 
 private:
     int nat;
@@ -91,18 +173,43 @@ int main()
                 a.mult(b);
             if (op == "sub")
                 a.sub(b);
+            cout << a.getReal() << " " << a.getImg() << endl;
         }
-        else if (type == "rational")
+        if (type == "rational")
         {
-        	string op;
-        	cin >> op;
-        	double c, d;
+            string op;
+            cin >> op;
+            double c, d;
             cin >> c >> d;
             Rational a(c, d);
+            if (op == "reduce")
+            {
+                a.reduce();
+                continue;
+            }
             cin >> c >> d;
             Rational b(c, d);
-
+            if (op == "add")
+                a.add(b);
+            if (op == "mult")
+                a.mult(b);
+            if (op == "sub")
+                a.sub(b);
+            cout << a.getReal() << endl;
         }
-
+        if (type == "natural")
+        {
+            string op;
+            cin >> op;
+            int c;
+            cin >> c;
+            Natural a(c);
+            if (op == "isprime")
+                a.isPrime();
+            if (op == "inverse")
+                a.inverse();
+        }
     }
+
+    return 0;
 }
