badd5f4ae1b15bf0622fe807a23eb85fe9b525bb
Soumith Basina <soumith.b@gmail.com>
2020-02-26 22:19:03 +0530

    updated A7

--- A5/template.cpp
+++ A5/template.cpp
@@ -1,76 +1,289 @@
 #include <iostream>
 #include <sstream>
 #include <vector>
+#include <string>
 using namespace std;
 
 template <typename T>
-class myStack {
+class myStack
+{
 public:
-	void push(T n);
-	void pop();
-	T top();
+    void push(T n)
+    {
+        stack.push_back(n);
+    };
+    void pop()
+    {
+        stack.pop_back();
+    };
+    T top()
+    {
+        return stack.back();
+    };
+    bool empty()
+    {
+        return stack.empty();
+    };
+    int nElements()
+    {
+        return stack.size();
+    };
 
 private:
-	vector<T> stack;
+    vector<T> stack;
 };
 
-class Polynomial {
+class Polynomial
+{
 public:
-	Polynomial();
-	void print();
-	Polynomial operator+(Polynomial P);
-	Polynomial operator-(Polynomial P);
-	Polynomial operator*(Polynomial P);
-	void takeInput();
+    Polynomial()
+    {
+        for (int i = 0; i < 1001; i++)
+        {
+            poly[i] = 0;
+        }
+    };
+    void print()
+    {
+        bool flag = false;
+        for (int i = 0; i < 1001; i++)
+        {
+            if (poly[i] != 0)
+            {
+                if (poly[i] < 0 && !flag)
+                {
+                    cout << "-";
+                }
+                else if (poly[i] > 0 && flag)
+                {
+                    cout << "+ ";
+                }
+                else if (poly[i] < 0 && flag)
+                {
+                    cout << "- ";
+                }
+                cout << abs(poly[i]) << "x^" << i << " ";
+                flag = true;
+            }
+        }
+        cout << endl;
+    };
+    Polynomial operator+(Polynomial p)
+    {
+        Polynomial out;
+        for (int i = 0; i < 1001; i++)
+            out.poly[i] = poly[i] + p.poly[i];
+        return out;
+    };
+    Polynomial operator-(Polynomial p)
+    {
+        Polynomial out;
+        for (int i = 0; i < 1001; i++)
+            out.poly[i] = poly[i] - p.poly[i];
+        return out;
+    };
+    Polynomial operator*(Polynomial p)
+    {
+        Polynomial out;
+        for (int i = 0; i < 1001; i++)
+        {
+            int j, k;
+            if (i < 501)
+            {
+                j = 0;
+                k = i;
+            }
+            else
+            {
+                j = i - 500;
+                k = 500;
+            }
+            for (; k >= 0 && j < 501; j++, k--)
+            {
+                out.poly[i] = out.poly[i] + (poly[j] * p.poly[k]);
+            }
+        }
+        return out;
+    };
+    void takeInput(int exp, int coeff)
+    {
+        poly[exp] = coeff;
+    };
 
 private:
-	vector <int> poly;
+    int poly[1001];
 };
 
-void takeIntInput() {
+void takeIntInput()
+{
 
-    std::string line, token;
-    std::cin.ignore();
-    std::getline(std::cin, line);
+    string line, token;
+    myStack<string> op;
+    myStack<long long int> exp;
 
-    std::stringstream ss(line);
-    
-    while(ss >> token) {
-        // process each token
-        // this will either be an operator, a parenthesis or an integer
+    cin.ignore();
+    getline(cin, line);
+
+    stringstream ss(line);
+
+    while (ss >> token)
+    {
+        if (token[0] == '(' || token[0] == '+' || token[0] == '-' || token[0] == '*')
+            op.push(token);
+        else if (token[0] <= '9' || token[0] >= '0')
+        {
+            if (!op.empty())
+            {
+                if (op.top() == "*")
+                {
+                    long long int i = stoi(token), j = exp.top();
+                    exp.pop();
+                    i = i * j;
+                    exp.push(i);
+                    op.pop();
+                }
+            }
+            else
+                exp.push(stoi(token));
+        }
+        else if (token[0] == ')')
+        {
+            while (true)
+            {
+                if (op.top() == "(")
+                {
+                    op.pop();
+                    while (!op.empty())
+                    {
+                        if (op.top() != "*")
+                            break;
+                        long long int i = exp.top(), j;
+                        exp.pop();
+                        j = exp.top();
+                        exp.pop();
+                        i = i * j;
+                        exp.push(i);
+                        op.pop();
+                    }
+                    break;
+                }
+
+                long long int i = exp.top(), j;
+                exp.pop();
+                j = exp.top();
+                exp.pop();
+
+                if (op.top() == "+")
+                    i = i + j;
+                else if (op.top() == "-")
+                    i = i - j;
+
+                exp.push(i);
+                op.pop();
+            }
+        }
     }
+
+    while (!op.empty())
+    {
+        long long int i = exp.top(), j;
+        exp.pop();
+        j = exp.top();
+        exp.pop();
+
+        if (op.top() == "+")
+            i = i + j;
+        else if (op.top() == "-")
+            i = i - j;
+
+        exp.push(i);
+        op.pop();
+    }
+    cout << exp.top() << endl;
 }
 
-void takePolyInput() {
+void takePolyInput()
+{
     int num;
-    std::cin >> num;
+    cin >> num;
 
-    std::string line;
+    string line;
+    myStack<string> op;
+    myStack<Polynomial> exp;
 
-    std::cin.ignore();
+    cin.ignore();
 
-    for(int i = 0; i < num; i++) {
-        std::getline(std::cin, line);
+    for (int i = 0; i < num; i++)
+    {
+        getline(cin, line);
 
-        if(line.size() == 1) {
-            // process the line
-            // this will be either an operator or a parenthesis
+        if (line.size() == 1)
+        {
+            if (line[0] == '(' || line[0] == '+' || line[0] == '-' || line[0] == '*')
+                op.push(line);
+            else if (line[0] == ')')
+            {
+                while (!op.empty())
+                {
+                    if (op.top() == "(")
+                    {
+                        op.pop();
+                        while (!op.empty())
+                        {
+                            if (op.top() != "*")
+                                break;
+                            Polynomial i, j = exp.top(), k;
+                            exp.pop();
+                            i = exp.top();
+                            exp.pop();
+                            k = i * j;
+                            exp.push(k);
+                            op.pop();
+                        }
+                        break;
+                    }
+
+                    Polynomial i = exp.top(), j, k;
+                    exp.pop();
+                    j = exp.top();
+                    exp.pop();
+
+                    if (op.top() == "+")
+                        k = i + j;
+                    else if (op.top() == "-")
+                        k = i - j;
+
+                    exp.push(k);
+                    op.pop();
+                }
+            }
         }
-        else {
+        else
+        {
             // this will be a polynomial
-            std::stringstream ss(line);
+            stringstream ss(line);
 
             int exponent;
             long long coeff;
-            while(ss >> exponent, ss >> coeff) {
+            while (ss >> exponent, ss >> coeff)
+            {
                 // process the exponent and coefficient appropriately
             }
-
         }
     }
 }
 
-int main() {
-	int n;
-
+int main()
+{
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; ++i)
+    {
+        string e_type;
+        cin >> e_type;
+        if (e_type == "int")
+            takeIntInput();
+        else if (e_type == "poly")
+            takePolyInput();
+    }
 }
--- A7/students.cpp
+++ A7/students.cpp
@@ -1,13 +1,126 @@
 #include <iostream>
+#include <string>
+#include <vector>
+#include <cstdlib>
 
-class Student {
-private:
+using namespace std;
+
+class Student
+{
+public:
 	int id;
-	int marks[5];
+	int m[5];
 	int skill;
 	int rank;
-}
+};
 
-int main() {
+class List
+{
+public:
+	vector<Student> list;
 
+	void insert(Student stu)
+	{
+		if (list.size() == 0)
+		{
+			list.push_back(stu);
+			return;
+		}
+
+		bool flag = false;
+		for (int i = 0; i < list.size(); i++)
+		{
+			for (int j = 0; j < 5; j++)
+			{
+				if (stu.m[j] < list[i].m[j])
+					break;
+				else if (stu.m[j] > list[i].m[j])
+				{
+					list.insert(list.begin() + i, stu);
+					flag = true;
+					return;
+				}
+			}
+		}
+		if (flag == false)
+			list.push_back(stu);
+	};
+	void updateRanks()
+	{
+		for (int i = 0; i < list.size(); i++)
+			list[i].rank = i + 1;
+	};
+	int find(int id1, int id2)
+	{
+		updateRanks();
+		int r1 = 0, r2 = 0;
+		for (int i = 0; i < list.size(); i++)
+		{
+			if (r1 * r2 == 0)
+			{
+				if (list[i].id == id1)
+					r1 = list[i].rank;
+				if (list[i].id == id2)
+					r2 = list[i].rank;
+			}
+		}
+		return abs(r1 - r2);
+	};
+	int count(int gid, int d)
+	{
+		int s, count = 0;
+		for (int i = 0; i < list.size(); i++)
+		{
+			if (list[i].id == gid)
+			{
+				s = list[i].skill;
+				break;
+			}
+		}
+		for (int i = 0; i < list.size(); i++)
+		{
+			if (abs(list[i].skill - s) > d)
+				count++;
+		}
+		return count;
+	};
+	void print()
+	{
+		for (int i = 0; i < list.size(); i++)
+			cout << list[i].id << " ";
+		cout << endl;
+	};
+};
+
+int main()
+{
+	string op;
+	List students;
+	cin >> op;
+	while (!cin.eof())
+	{
+		if (op == "ADD")
+		{
+			Student temp;
+			cin >> temp.id >> temp.m[0] >> temp.m[1] >> temp.m[2] >> temp.m[3] >> temp.m[4] >> temp.skill;
+			students.insert(temp);
+		}
+		else if (op == "PRINT")
+		{
+			students.print();
+		}
+		else if (op == "COUNT")
+		{
+			int t1, t2;
+			cin >> t1 >> t2;
+			cout << students.count(t1, t2) << endl;
+		}
+		else if (op == "FIND")
+		{
+			int t1, t2;
+			cin >> t1 >> t2;
+			cout << students.find(t1, t2) << endl;
+		}
+		cin >> op;
+	}
 }
